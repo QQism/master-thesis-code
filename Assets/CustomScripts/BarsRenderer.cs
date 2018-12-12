0882ef3f8f99d2bd68680a0747586c214a487242
QQ <me@quang.be>
2018-12-13 00:17:24 +1100

    Add DividedQuad prefab

---
+++ Assets/CustomScripts/BarsRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarsRenderer : MonoBehaviour {
+
+	public GameObject _quad;
+
+	private List<GameObject> bars = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject bar = Instantiate(_quad, Vector3.zero, Quaternion.identity); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

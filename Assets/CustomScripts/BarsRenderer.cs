8e01b95e2e5067203b29e7683ff1aecbe519efda
QQ <nqua0002@student.monash.edu>
2018-12-13 14:08:31 +1100

    [WIP] Update

--- Assets/CustomScripts/BarsRenderer.cs
+++ Assets/CustomScripts/BarsRenderer.cs
@@ -6,12 +6,25 @@ public class BarsRenderer : MonoBehaviour {
 
 	public GameObject _quad;
 
+	private int _quadsCount = 4;
+
 	private List<GameObject> bars = new List<GameObject>();
 
 	// Use this for initialization
 	void Start ()
 	{
-		GameObject bar = Instantiate(_quad, Vector3.zero, Quaternion.identity); 
+		float rotateAngle = 360.0f / _quadsCount;
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject bar = Instantiate(_quad, transform.position, Quaternion.identity);
+			bar.name = "Bar " + i.ToString();
+            bar.transform.SetParent(transform);
+            Material quadMaterial = new Material(_quad.GetComponent<Renderer>().sharedMaterial);
+            bar.GetComponent<Renderer>().sharedMaterial = quadMaterial;
+            quadMaterial.SetFloat("_UpperScale", 1.0f);
+            quadMaterial.SetFloat("_LowerScale", 0.0f);
+            bar.transform.Rotate(0, i*rotateAngle, 0);
+        }
 	}
 	
 	// Update is called once per frame

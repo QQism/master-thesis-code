c76af5fb50868168d6487d20c43071e5053be50c
QQ <me@quang.be>
2018-12-16 21:11:56 +1100

    Add rotation and translate of building the pyramid

--- Assets/CustomScripts/BarsRenderer.cs
+++ Assets/CustomScripts/BarsRenderer.cs
@@ -6,34 +6,41 @@ public class BarsRenderer : MonoBehaviour {
 
 	public GameObject _quad;
 
+	[Range(0, 90)]
+	public float _miterAngle = 45;
+
 	private int _quadsCount = 4;
+
+	private float _faceHeight = 1.0f;
 
 	private List<GameObject> bars = new List<GameObject>();
 
 	// Use this for initialization
 	void Start ()
 	{
-		float rotateAngle = 360.0f / _quadsCount;
-        for (int i = 0; i < 4; i++)
+		float rotateYAngle = 360.0f / _quadsCount;
+        for (int i = 0; i < _quadsCount; i++)
         {
             GameObject bar = Instantiate(_quad, transform.position, Quaternion.identity);
 			bar.name = "Bar " + i.ToString();
             bar.transform.SetParent(transform);
             Material quadMaterial = new Material(_quad.GetComponent<Renderer>().sharedMaterial);
             bar.GetComponent<Renderer>().sharedMaterial = quadMaterial;
-            quadMaterial.SetFloat("_UpperScale", 1.0f);
+
+			float miterRadAngle = _miterAngle * Mathf.Deg2Rad;
+			float baseRadius = _faceHeight * Mathf.Sin(miterRadAngle);
+			float upperScale = baseRadius * 2;
+			float height = _faceHeight * Mathf.Sin(Mathf.PI/2 - miterRadAngle);
+
+
+            quadMaterial.SetFloat("_UpperScale", upperScale);
             quadMaterial.SetFloat("_LowerScale", 0.0f);
-            bar.transform.Rotate(0, i*rotateAngle, 0);
-			bar.transform.Rotate(63.434f, 0, 0);
-			bar.transform.localPosition += new Vector3(0, 0, 0.5f);
+			bar.transform.localPosition += bar.transform.TransformDirection(bar.transform.forward) * baseRadius/2.0f;
+			bar.transform.RotateAround(Vector3.zero, Vector3.up, i * rotateYAngle);
+			bar.transform.Rotate(Vector3.right * _miterAngle, Space.Self);
         }
 	}
 
-	float calculateUpperScale(float S, float alphaAngle)
-	{
-		return Mathf.Sqrt(2 * Mathf.Pow(S, 2) * (1 - Mathf.Cos(alphaAngle)));
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		

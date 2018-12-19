ee17a821c2635b1cb50bda6f14bc83c666368183
QQ <nqua0002@student.monash.edu>
2018-12-20 10:47:31 +1100

    Cone follows the Camera

---
+++ Assets/CustomScripts/CameraAttachedBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAttachedBehavior : MonoBehaviour {
+
+	public Camera _mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Scale(_mainCamera.transform.position, new Vector3(1, 0.95f, 1));
+	}
+}

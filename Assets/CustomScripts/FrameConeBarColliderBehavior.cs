09b21f30d4a3ff512e3f38f9450ab31a3443f0d5
QQ <nqua0002@student.monash.edu>
2019-04-29 19:47:47 +1000

    [WIP] Handle selection

---
+++ Assets/CustomScripts/FrameConeBarColliderBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameConeBarColliderBehavior : MonoBehaviour {
+
+	void Awake()
+	{
+		var boxCollider = gameObject.GetComponent<BoxCollider>();
+		boxCollider.isTrigger = true;		
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//Debug.Log("OnTriggerEnter");
+		//gameObject
+	}
+
+	private void OnTriggerExit(Collider other)
+	{	
+		//Debug.Log("OnTriggerExit");
+	}
+}

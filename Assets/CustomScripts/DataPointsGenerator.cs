fe12e4e475c451d886c48ad279bb26e1fbce0578
QQ <nqua0002@student.monash.edu>
2019-05-06 18:30:00 +1000

    Add Text Mesh Pro

---
+++ Assets/CustomScripts/DataPointsGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPointsGenerator : MonoBehaviour {
+
+	private int numberDataPoints = 0;
+	private int closestDistance = 0;
+	private int furthestDistance = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

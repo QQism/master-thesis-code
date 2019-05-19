71230b059afbad1a165c44a3df0c14faaa23bca3
QQ <nqua0002@student.monash.edu>
2019-05-19 13:17:28 +1000

    Support multi datasets & one more of questions

--- Assets/CustomScripts/userstudy/Question.cs
+++ Assets/CustomScripts/userstudy/Question.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public enum Task
 {
 	EstimateSinglePoint,
-	PickLargerDataPoint
+	PickLargerDataPoint,
+	PickCloserDataPoint,
+}
+
+public enum Dataset
+{
+	Dataset1,
+	Dataset2,
+	Dataset3,
 }
 public class Question
 {
@@ -13,19 +21,20 @@ public class Question
 	public Task task;
 	public VisualisationType visualisationType;
 	//public List<MapDataPoint> dataPoints;
+	public Dataset dataset;
 	public int dataPoint1Idx;
 	public int dataPoint2Idx;
 	public int dataPoint3Idx;
 
-	public Question(int id, Task task, VisualisationType visualisationType, int dataPoint1Idx): this(id, task, visualisationType, dataPoint1Idx, -1)
-	{
-		
-	}
-	public Question(int id, Task task, VisualisationType visualisationType, int dataPoint1Idx, int dataPoint2Idx)
+    public Question(int id, Task task, VisualisationType visualisationType, Dataset dataset, int dataPoint1Idx)
+    : this(id, task, visualisationType, dataset, dataPoint1Idx, -1)
+    { }
+	public Question(int id, Task task, VisualisationType visualisationType, Dataset dataset, int dataPoint1Idx, int dataPoint2Idx)
 	{
 		this.id = id;
 		this.task = task;
 		this.visualisationType = visualisationType;	
+		this.dataset = dataset;
 		this.dataPoint1Idx = dataPoint1Idx;
 		this.dataPoint2Idx = dataPoint2Idx;
 	}

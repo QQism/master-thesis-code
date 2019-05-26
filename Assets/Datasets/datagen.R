9e502d40eeb4d0757758d5919335594d2bd60a5b
QQ <me@quang.be>
2019-05-26 16:44:11 +1000

    Update datagen for task 2

--- Assets/Datasets/datagen.R
+++ Assets/Datasets/datagen.R
@@ -105,71 +105,146 @@ generate_task1_dataset()
 # Each vis: 1-18-37
 generate_task2_dataset <- function()
 {
-  task2_repetition <- 1
+  task2_repetition <- 3
   vis_levels <- 3
   radius_levels <- 3
   fov_levels <- 2
-  datasets_count <- task2_repetition * vis_levels * radius_levels * fov_levels
-  angles1 <- randomNumbers(n=datasets_count, min=1, max=360, col=1)
-  currentAngleIdx <- 1
+  datasets_count <- task2_repetition * vis_levels * radius_levels * fov_levels * 2
   
+  questions <- data.frame("id"=integer(), 
+                          "vis"=character(), 
+                          "radius1"=double(), "radius2"=double(),
+                          "fov"=integer(), 
+                          "angle1"=integer(), "angle2"=integer(),
+                          "re"=integer(), stringsAsFactors = F,
+                          "answer"=integer())
+  
+  for (id in 1:(datasets_count/2))
+  {
+    questions[id, 1] <- id
+  }
+  
+  questions[1:(datasets_count/2/3), 2] <- "v1"
+  questions[((datasets_count/2/3)+1):(datasets_count/2/3*2), 2] <- "v2"
+  questions[(datasets_count/2/3*2+1):(datasets_count/2), 2] <- "v3"
+  
+  # Generate datasets_count number of close radius
+  sCloseRadii <- runif(datasets_count/2, min=minRadius, max=closeRadius)
+  closeRadiusIdx <- 1
+  # Generate datasets_count number of far radius
+  sFarRadii <- runif(datasets_count/2, min=closeRadius, max=maxRadius)
+  farRadiusIdx <- 1
+  
+  # FOV
   max_in_fov <- 110
   min_in_fov <- 5
   max_out_fov <- 179
-
-  # Generate datasets_count number of close radius
-  closeRadius <- runif(datasets_count, min=minRadius, max=closeRadius)
-  closeRadiusIdx <- 1
-  # Generate datasets_count number of far radius
-  farRadius <- runif(datasets_count, min=closeRadius, max=maxRadius)
-  farRadiusIdx <- 1
   
-  dataPoints <- data.frame("id"=integer(), "x"=double(), "y"=double(), "value"=double(), stringsAsFactors = F)
   # Loop for in_fov
-  in_fovs <- randomNumbers(n=datasets_count/2, min=min_in_fov, max=max_in_fov, col=1)
-  for (fov in in_fovs)
+  # n = c/2/3/2 (number of questions is half of datapoints, 3 vis share the same fov, 2 levels of fov)
+  in_fovs <- randomNumbers(n=datasets_count/2/3/2, min=min_in_fov, max=max_in_fov, col=1)
+  currentInFovIdx <- 1
+  # Loop for out_fov
+  out_fovs <- randomNumbers(n=datasets_count/2/3/2, min=max_in_fov, max=max_out_fov, col=1)
+  currentOutFovIdx <- 1
+  
+  for (rep in 0:(task2_repetition-1))
   {
-    # Each vis: same fov, same radii, different angles
-    # Radius levels: C-C C-F F-F
+    # CC
+    for (r in 0:1)
+    {
+      radius1 <- sCloseRadii[closeRadiusIdx]
+      closeRadiusIdx <- closeRadiusIdx + 1
+      radius2 <- sCloseRadii[closeRadiusIdx]
+      closeRadiusIdx <- closeRadiusIdx + 1
+      for (id in 1:3)
+      {
+        questions[id*6*task2_repetition-(6 * task2_repetition - 1)+r*task2_repetition+rep, 3:4] <- c(radius1, radius2)
+      }
+    }
     
-    # C-C
-    cc_startIdx = 1
-    #for (r in 1:datasets_count/3)
-    #{
-      radius1 <- closeRadius[closeRadiusIdx]
+    # CF
+    for (r in 0:1)
+    {
+      radius1 <- sCloseRadii[closeRadiusIdx]
       closeRadiusIdx <- closeRadiusIdx + 1
-      radius2 <- closeRadius[closeRadiusIdx]
-      closeRadiusIdx <- closeRadiusIdx + 1
+      radius2 <- sFarRadii[farRadiusIdx]
+      farRadiusIdx <- farRadiusIdx + 1  
       
-      for (idx in 1:3)
-      { 
-        angle1 = angles1[currentAngleIdx]
-        currentAngleIdx <- currentAngleIdx + 1
-        
-        angle2 <- angle1 - fov
-        
-        x1 <- sin(angle1 * pi/180) * radius1
-        y1 <- cos(angle1 * pi/180) * radius1
-        id1 <- idx * 18 - 17
-        dataPoints[id1, ] <- c(id1, x1, y1, 0)
-        
-        x2 <- sin(angle2 * pi/180) * radius2
-        y2 <- cos(angle2 * pi/180) * radius2
-        id2 <- id1 + 1
-        dataPoints[id2, ] <- c(id2, x2, y2, 0)
-      #}
+      for (id in 1:3)
+      {
+        questions[id*6*task2_repetition-(6 * task2_repetition - 1)+(task2_repetition*2)+r*task2_repetition+rep, 3:4] <- c(radius1, radius2)
+      }
+    }
+    
+    # FF
+    for (r in 0:1)
+    {
+      radius1 <- sFarRadii[farRadiusIdx]
+      farRadiusIdx <- farRadiusIdx + 1
+      radius2 <- sFarRadii[farRadiusIdx]
+      farRadiusIdx <- farRadiusIdx + 1
+      
+      for (id in 1:3)
+      {
+        questions[id*6*task2_repetition-(6 * task2_repetition - 1)+(task2_repetition*2*2)+r*task2_repetition+rep, 3:4] <- c(radius1, radius2)
+      }
+    }
+    
+    # Loop in fov
+    for (r in 0:2)
+    {
+      fov <- in_fovs[currentInFovIdx]
+      currentInFovIdx <- currentInFovIdx + 1
+      for (id in 1:3)
+      {
+        idx <- id*6*task2_repetition-(6 * task2_repetition - 1)+r*2*task2_repetition+rep
+        questions[idx, 5] <- fov
+        questions[idx, 8] <- rep+1
+      }
+    }
+    
+    for (r in 0:2)
+    {
+      fov <- out_fovs[currentOutFovIdx]
+      currentOutFovIdx <- currentOutFovIdx + 1
+      for (id in 1:3)
+      {
+        idx <- id*6*task2_repetition-(6 * task2_repetition - 1)+r*2*task2_repetition+task2_repetition+rep
+        questions[idx, 5] <- fov
+        questions[idx, 8] <- rep+1
+      }
     }
   }
   
-  # Loop for out_fov
-  out_fovs <- randomNumbers(n=datasets_count/2, min=max_in_fov, max=max_out_fov, col=1)
-  
-  # Create pairs of radius
-  
-  # Each of pair will be used for 3 types of vis
-  # For each type of vis, randomise the angle for
-  
-  # In fov
-  # 
-  
+  dataPoints <- data.frame("id"=integer(), "x"=double(), "y"=double(), "value"=double(), stringsAsFactors = F)
+  angles1 <- randomNumbers(n=datasets_count/2, min=1, max=360, col=1)
+  currentAngleIdx <- 1
+  # Angles1
+  for (id in 1:(datasets_count/2))
+  {
+    radius1 <- questions[id,"radius1"]
+    radius2 <- questions[id,"radius2"]
+    fov <- questions[id, "fov"]
+    angle1 <- angles1[currentAngleIdx]
+    currentAngleIdx <- currentAngleIdx + 1
+    
+    x1 <- sin(angle1 * pi/180) * radius1
+    y1 <- cos(angle1 * pi/180) * radius1
+    
+    angle2 <- angle1 - fov
+    x2 <- sin(angle2 * pi/180) * radius2
+    y2 <- cos(angle2 * pi/180) * radius2
+    
+    questions[id, 6:7] <- c(angle1, angle2)
+    
+    if ((id %% 2) == 1)
+    {
+      questions[id, 9] <- 1
+    } else
+    { 
+      questions[id, 9] <- 2
+    }
+  }
 }
+
